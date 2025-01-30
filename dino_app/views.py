ecc67f858835f4a4e29ea7286dece36651e5b79e
agaolejniq <a.olejnikk@gmail.com>
2025-01-30 16:46:11 +0100

    finished moving gpx saving to the views, deleted obsolete js script

--- dino_app/views.py
+++ dino_app/views.py
@@ -1,5 +1,9 @@
+import os
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+import rasterio
+
+from dino import settings
 from .models import Route
 from .forms import RouteForm
 import json
@@ -37,10 +41,6 @@ def routes(request):
 def route(request, route_id):
     """Show a single route and its details"""
     route = Route.objects.get(id=route_id)
-    print(type(route.waypoints_list))
-    print(f"Trackpoints loaded: {route.trackpoints_list[:5]}")
-    print(type(route.trackpoints_list))
-
     context = {'route' : route}
     return render(request, 'dino_app/route.html', context)
     
@@ -50,11 +50,11 @@ def new_route(request):
     if request.method != 'POST':
         # Create blank form
         form = RouteForm()
-        print(request.POST)
+        # print(request.POST)
     else:
         # Prcess POST data
         form = RouteForm(data=request.POST)
-        print(request.POST)
+        # print(request.POST)
         if form.is_valid():
             form.save()
             return redirect('dino_app:routes')
@@ -75,6 +75,7 @@ def dummy(request):
 def save_gpx(request, route_id):
     """Generate gpx file with route details"""
     route = Route.objects.get(id=route_id)
+    parsed_trackpoints = parse_trackpoints(route.trackpoints_list)
     gpx_template = f'''<?xml version="1.0" encoding="UTF-8"?>
 <gpx xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
      xsi:schemaLocation="http://www.topografix.com/GPX/1/1 http://www.topografix.com/GPX/1/1/gpx.xsd
@@ -89,19 +90,45 @@ def save_gpx(request, route_id):
     <trk>
         <name>{route.title}</name>
         <trkseg>
-            {parse_trackpoints(json.loads(route.trackpoints_list))}
+            {parsed_trackpoints}
         </trkseg>
     </trk>
 </gpx>'''.strip()
-    return gpx_template
+
+    response = HttpResponse(gpx_template, content_type="application/gpx+xml")
+    response['Content-Disposition'] = f'attachment; filename="{route.title}.gpx"'
+
+    return response
 
 def parse_trackpoints(trackpoints_json):
     """Convert trackpoints to XML format"""
+
     parsed_string=""
     for trackpoint in trackpoints_json:
+        elevation = extract_elevation(trackpoint)
         parsed_string += f'''
-<trkpt lat="{trackpoint["lat"]}" lon="{trackpoint["lng"]}">
-    <ele></ele>
-    <time></time>
-</trkpt>'''
+            <trkpt lat="{trackpoint["lat"]}" lon="{trackpoint["lng"]}">
+                <ele>{elevation}</ele>
+                <time></time>
+            </trkpt>'''
     return parsed_string
+
+def extract_elevation(trackpoint):
+    """Extract elevation data from trackpoints using DEM"""
+    dem_file = os.path.join(settings.BASE_DIR, 'static', 'dino_app', 'geotiff_data', 'GDEM-10km-BW.tif') # GDEM-10km-BW.tif ebk1km1.tif
+    lat, lon = trackpoint["lat"], trackpoint["lng"]
+
+    with rasterio.open(dem_file) as dataset:
+        # Get the affine transformation from geospatial coordinates to pixel coordinates
+        transform = dataset.transform
+
+        # Convert latitude/longitude (WGS84) to pixel coordinates (row, col)
+        col, row = ~transform * (lon, lat)  # ~transform is the inverse of the affine transform
+        
+        # Make sure the row/col are within bounds
+        if 0 <= col < dataset.width and 0 <= row < dataset.height:
+            # Read the elevation at the specified row/col
+            elevation = dataset.read(1, window=rasterio.windows.Window(col, row, 1, 1))[0]
+            return elevation[0]
+        else:
+            return None  # If the coordinates are out of bounds
